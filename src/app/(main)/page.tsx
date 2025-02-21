3789ed92ff3f26ac9d1159ec3d11db64dcb839aa
Arshit Chaurasia <arshitcc@gmail.com>
2025-02-22 02:42:36 +0530

    feat : Add Clerk authentication

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Image from "next/image";
+import { SignOutButton, useAuth } from '@clerk/nextjs'
 
 export default function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="bg-green-400 px-8 py-4 rounded-xl ">
+        <SignOutButton/>
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
